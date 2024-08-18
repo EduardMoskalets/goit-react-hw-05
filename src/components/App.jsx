f9726e16ea0bc5c66fb52f465bcb611ea68ffb30
Eduard <edik.moskalets@gmail.com>
2024-08-18 12:43:40 +0300

    app + main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from '../components/Navigation/Navigation';
 import './App.module.css'
+
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage/MovieDetailsPage'));
+const MovieCast = lazy(() => import('../components/MovieCast/MovieCast'));
+const MovieReviews = lazy(() => import('../components/MovieReviews/MovieReviews'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
+
+export default function App() {
+  return (
+    <div className={css.container}>
+      <Navigation />
+
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
 
 
